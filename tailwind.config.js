657633f1db399463162beb7332cdfdb6836ff690
Mamii868 <marcmotenii@gmail.com>
2024-07-15 11:45:32 -0500

    Add default font and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,20 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    screens: {},
+    colors: {
+      'hotPink': '#ff5781',
+      'lightPink': '#ffd6e0',
+      'pink': '#fa94ae',
+      'white': '#ffebf0',
+      'black': '#554e50',
+      'green': '#588b56',
+      'lightGreen': '#53a96c',
+      'blue': '#242547',
+    },
+    fontFamily: {
+      poppins: ['Poppins','Roboto Thin', 'sans-serif'],
+    },
     extend: {},
   },
   plugins: [],
